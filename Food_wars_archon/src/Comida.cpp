179815292a6b6fc6d82e6dd37c0c44866d92c387
martaredondoperez <marta.redondo.perez@alumnos.upm.es>
2026-05-06 17:12:46 +0200

    inicio de meter sprites sequence personajes

--- Food_wars_archon/src/Comida.cpp
+++ Food_wars_archon/src/Comida.cpp
@@ -1,6 +1,7 @@
 #include "Comida.h"
 #include "freeglut.h"
 #include "Definiciones.h"
+#include "ETSIDI.h"
 
 // Constructor
 Comida::Comida(Bando b, TipoFicha t, int f, int c) {
@@ -8,6 +9,7 @@ Comida::Comida(Bando b, TipoFicha t, int f, int c) {
     tipo = t;
     fila = f;
     columna = c;
+    sprite = NULL;
 
     // Asignación de estadísticas según el tipo de tropa
     switch (tipo) {
@@ -16,6 +18,9 @@ Comida::Comida(Bando b, TipoFicha t, int f, int c) {
         vidaMax = 100;
         ataque = 25;
         rangoMovimiento = 3;
+        //carga de sprite sequence
+        if (bando == SALUDABLE) sprite = new ETSIDI::SpriteSequence("imagenes/LIDER_Chef_Michelin.png", 4, 1, 150);
+        else sprite = new ETSIDI::SpriteSequence("imagenes/LIDER_Rey_Hamburguesa.png", 4, 1, 150);
         break;
 
     case PESADA:
@@ -23,6 +28,9 @@ Comida::Comida(Bando b, TipoFicha t, int f, int c) {
         vidaMax = 150;       // Muchísima vida
         ataque = 20;         // Pega duro cuerpo a cuerpo
         rangoMovimiento = 2; // Muy lentos
+        //carga de sprite sequence
+        if (bando == SALUDABLE) sprite = new ETSIDI::SpriteSequence("imagenes/PESADA_Aguacate_Escudo.png", 4, 1, 150);
+        else sprite = new ETSIDI::SpriteSequence("imagenes/PESADA_Hamburguesa_Triple.png", 4, 1, 150);
         break;
 
     case VOLADORA:
@@ -30,6 +38,9 @@ Comida::Comida(Bando b, TipoFicha t, int f, int c) {
         vidaMax = 50;        // Poca vida 
         ataque = 15;
         rangoMovimiento = 5; // Pueden cruzar medio tablero rápido
+        //carga de sprite sequence
+        if (bando == SALUDABLE) sprite = new ETSIDI::SpriteSequence("imagenes/VOLADORA_Rodaja_Limon.png", 4, 1, 150);
+        else sprite = new ETSIDI::SpriteSequence("imagenes/VOLADORA_Pizza_Pepperoni.png", 4, 1, 150);
         break;
 
     case DISTANCIA:
@@ -37,6 +48,9 @@ Comida::Comida(Bando b, TipoFicha t, int f, int c) {
         vidaMax = 40;        // Muy poca vida, mueren rápido
         ataque = 10;         // Daño bajo en tablero
         rangoMovimiento = 3; // Movimiento estándar
+        //carga de sprite sequence
+        if (bando == SALUDABLE) sprite = new ETSIDI::SpriteSequence("imagenes/DISTANCIA_Chorro_Vitaminas.png", 4, 1, 150);
+        else sprite = new ETSIDI::SpriteSequence("imagenes/DISTANCIA_Disparo_Ketchup.png", 4, 1, 150);
         break;
 
     case ESPECIAL:
@@ -44,6 +58,9 @@ Comida::Comida(Bando b, TipoFicha t, int f, int c) {
         vidaMax = 80;        // Vida media
         ataque = 30;         // Tienen el ataque más fuerte 
         rangoMovimiento = 4; // Muy ágiles
+        //carga de sprite sequence
+        if (bando == SALUDABLE) sprite = new ETSIDI::SpriteSequence("imagenes/ESPECIAL_Jengibre_Teleport.png", 4, 1, 150);
+        else sprite = new ETSIDI::SpriteSequence("imagenes/ESPECIAL_Helado_Shapeshifter.png", 4, 1, 150);
         break;
 
     default:
@@ -53,32 +70,51 @@ Comida::Comida(Bando b, TipoFicha t, int f, int c) {
         rangoMovimiento = 3;
         break;
     }
+    // Ajuste de tamaño del sprite para que encaje en la casilla
+    if (sprite) {
+        sprite->setSize(45, 45);
+    }
 
     // Al nacer, la ficha siempre tiene la vida a tope
     vidaActual = vidaMax;
 }
 
+Comida::~Comida() {
+    delete sprite;
+}
+
 // Función de dibujo temporal (cuadrados de colores para hacer pruebas)
 void Comida::dibuja(float xMin, float yMin, float lado) {
-    float centroX = xMin + (lado / 2.0f);
-    float centroY = yMin + (lado / 2.0f);
-    float radioFicha = lado * 0.3f;
+    // Si por algún motivo no cargó el sprite, dibujamos el cuadrado antiguo para no ver nada vacío
+    if (sprite == NULL) {
+        float centroX = xMin + (lado / 2.0f);
+        float centroY = yMin + (lado / 2.0f);
+        sprite->setPos(centroX, centroY);
+        sprite->draw();
+        float radioFicha = lado * 0.3f;
+        if (bando == SALUDABLE) glColor3ub(0, 150, 0);
+        else glColor3ub(150, 0, 0);
+        glBegin(GL_QUADS);
+        glVertex2f(centroX - radioFicha, centroY - radioFicha);
+        glVertex2f(centroX + radioFicha, centroY - radioFicha);
+        glVertex2f(centroX + radioFicha, centroY + radioFicha);
+        glVertex2f(centroX - radioFicha, centroY + radioFicha);
+        glEnd();
+    }
+    else {
+        glColor3ub(255, 255, 255);
+        glEnable(GL_TEXTURE_2D);
+        // Dibujamos el Sprite centrado en la casilla
+        sprite->setPos(xMin + lado / 2.0f, yMin + lado / 2.0f);
+        sprite->draw();
+        glDisable(GL_TEXTURE_2D);
+    }
+}
 
-    // Colores base por bando
-    if (bando == SALUDABLE) glColor3ub(0, 150, 0); // Verde para la comida sana
-    else glColor3ub(150, 0, 0);                    // Rojo para la comida rápida
-
-    // Detalles visuales por tipo para distinguirlos en tu pantalla
-    if (tipo == LIDER) glColor3ub(255, 215, 0);         // Dorado (Líder)
-    else if (tipo == PESADA) glColor3ub(100, 100, 100); // Gris oscuro (Tanque)
-    else if (tipo == DISTANCIA) glColor3ub(100, 200, 255); // Azul claro (Tirador)
-
-    glBegin(GL_QUADS);
-    glVertex2f(centroX - radioFicha, centroY - radioFicha);
-    glVertex2f(centroX + radioFicha, centroY - radioFicha);
-    glVertex2f(centroX + radioFicha, centroY + radioFicha);
-    glVertex2f(centroX - radioFicha, centroY + radioFicha);
-    glEnd();
+void Comida::actualiza(float t) {
+    if (sprite) {
+        sprite->loop(); // Esto cambia al siguiente frame de tus 4 imágenes
+    }
 }
 //  FUNCIONES PENDIENTES 
 bool Comida::intentarMover(int nuevaFila, int nuevaColumna) {
